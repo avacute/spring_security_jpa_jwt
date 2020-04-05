3ee48ce0a846fb28b6b7f8b148b610be058fc445
Fola Akinbobola <voachamber@gmil.com>
2020-04-05 14:59:24 +0100

    Commence auth-services dev

---
+++ src/test/java/io/opeleh/authenticationservices/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.opeleh.authenticationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
